caa1722f7d82b911bb17a97358030b79159236ac
raomaiping <2582395486@qq.com>
2021-08-19 00:31:32 +0800

    播放器内核组件开发

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <my-header></my-header>
   <tab></tab>
   <router-view></router-view>
+  <player></player>
 </template>
 
 <script>
 import MyHeader from '@/components/header/header'
 import Tab from '@/components/tab/tab'
+import Player from './components/player/player'
 export default {
   components: {
     MyHeader,
-    Tab
+    Tab,
+    Player
   }
 }
 </script>
